812f7d6b600b484ba48e5896b1d22fe0ff4ec098
Ajibade David <davidajibade58@gmail.com>
2023-05-30 10:27:47 +0100

    work

---
+++ signup.js
@@ -0,0 +1,18 @@
+function signUp(){
+    let firstName = firstName
+    let lastName = lastName
+    let email = email
+    let phoneNumber = phoneNumber
+    let userName = userName
+    let password = password
+    // let paragraph1= document.getElementById("screenSignUp")
+if (!firstName && !lastName && !email && !phoneNumber && !userName && !password) {
+    alert("Please fill all fields")
+    // paragraph1.innerHTML="Pls fill all fields"
+} else {
+    
+}
+
+    return val  
+
+}
